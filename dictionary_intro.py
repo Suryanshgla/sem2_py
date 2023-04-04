63aba1b25db3c30b63f3f9cf3dab84e00688a40d
Suryanshgla <127819373+Suryanshgla@users.noreply.github.com>
2023-04-03 22:01:17 -0700

    Update dictionary_intro.py

--- dictionary_intro.py
+++ dictionary_intro.py
@@ -1,7 +1,7 @@
-
 dict = {'NAME' : 'Suryansh', 'ROLL NO.' : 63, 'SECTION' : 'J'}
 print("name of the students = ",dict['NAME'])
 print(dict)
 
 dict['COURSE'] = 'BTECH'
+print(len(dict))
 print(dict)
